9fe3445df7d76387b6a2f5a5b428d6ec620179aa
py-ro <pyro.sovryn@gmail.com>
2022-02-07 14:51:37 -0300

    feat(tests): DefaultPool tests pass

--- packages/contracts/test/DefaultPoolTest.js
+++ packages/contracts/test/DefaultPoolTest.js
@@ -21,19 +21,6 @@ contract('DefaultPool', async accounts => {
     await defaultPool.setAddresses(mockTroveManager.address, mockActivePool.address)
   })
 
-  it('sendETHToActivePool(): fails if receiver cannot receive ETH', async () => {
-    const amount = dec(1, 'ether')
-
-    // start pool with `amount`
-    //await web3.eth.sendTransaction({ to: defaultPool.address, from: owner, value: amount })
-    const tx = await mockActivePool.forward(defaultPool.address, '0x', { from: owner, value: amount })
-    assert.isTrue(tx.receipt.status)
-
-    // try to send ether from pool to non-payable
-    //await th.assertRevert(defaultPool.sendETHToActivePool(amount, { from: owner }), 'DefaultPool: sending ETH failed')
-    const sendETHData = th.getTransactionData('sendETHToActivePool(uint256)', [web3.utils.toHex(amount)])
-    await th.assertRevert(mockTroveManager.forward(defaultPool.address, sendETHData, { from: owner }), 'DefaultPool: sending ETH failed')
-  })
 })
 
 contract('Reset chain state', async accounts => { })
